5b5fff1e5a7cb5f17362f5a2413e2918d9ff6e3d
스프링 피바라기 <daehwan7989@naver.com>
2023-01-01 01:18:14 +0900

    button 기반 binder 구성

---
+++ app/src/main/java/com/sopt/smeme/presentation/view/BinderActivity.kt
@@ -0,0 +1,17 @@
+package com.sopt.smeme.presentation.view
+
+import android.content.Intent
+import android.os.Bundle
+import com.sopt.smeme.R
+import com.sopt.smeme.databinding.ActivityBinderBinding
+
+class BinderActivity : ViewBoundActivity<ActivityBinderBinding>(R.layout.activity_binder) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.btnLoginActivity.setOnClickListener {
+            val toTestPage = Intent(this, TestActivity::class.java)
+            startActivity(toTestPage)
+        }
+    }
+}
