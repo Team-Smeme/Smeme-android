c23f6d3ab2a875867c1d65286245e3bfa2da42d0
스프링 피바라기 <daehwan7989@naver.com>
2023-01-01 01:18:14 +0900

    button 기반 binder 구성

---
+++ app/src/main/java/com/sopt/smeme/presentation/view/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.sopt.smeme.presentation.view
+
+import android.os.Bundle
+import com.sopt.smeme.R
+import com.sopt.smeme.databinding.ActivityTestBinding
+
+class TestActivity : ViewBoundActivity<ActivityTestBinding>(R.layout.activity_test) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
