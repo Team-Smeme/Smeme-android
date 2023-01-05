496fa6ad433099224cf6d8463871315dbd101500
스프링 피바라기 <daehwan7989@naver.com>
2023-01-05 22:03:47 +0900

    set Local Storage by SharedPreference

---
+++ app/src/main/java/com/sopt/smeme/system/storage/StorageModule.kt
@@ -0,0 +1,14 @@
+package com.sopt.smeme.system.storage
+
+/*
+@Module
+@InstallIn(SingletonComponent::class)
+class StorageModule (
+    @ApplicationContext context: Context
+){
+    @Provides
+    @Singleton
+    @Named("sharedPreference")
+    fun provideSharedPreference(): LocalStorage =
+        LocalSharedPreference(context = context)
+}*/
