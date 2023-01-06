9970f61ed738da6d93d12949e2f5e4fd92b4cda3
스프링 피바라기 <daehwan7989@naver.com>
2023-01-06 12:20:31 +0900

    add KakaoApiClient

--- app/src/main/java/com/sopt/smeme/Smeme.kt
+++ app/src/main/java/com/sopt/smeme/Smeme.kt
@@ -1,11 +1,15 @@
 package com.sopt.smeme
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.sopt.smeme.BuildConfig.KAKAO_API_KEY
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class Smeme : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        KakaoSdk.init(this, KAKAO_API_KEY)
     }
 }
