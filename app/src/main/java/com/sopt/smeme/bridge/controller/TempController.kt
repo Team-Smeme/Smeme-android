e80083f880695983504e57251dfc881e98810945
스프링 피바라기 <daehwan7989@naver.com>
2023-01-06 11:56:53 +0900

    delete Temp implementation

--- app/src/main/java/com/sopt/smeme/bridge/controller/TempController.kt
+++
@@ -1,5 +0,0 @@
-package com.sopt.smeme.bridge.controller
-
-interface TempController {
-    // api
-}
--- app/src/main/java/com/sopt/smeme/presentation/view/TestActivity.kt
+++ app/src/main/java/com/sopt/smeme/presentation/view/TestActivity.kt
@@ -1,12 +1,8 @@
 package com.sopt.smeme.presentation.view
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import com.sopt.smeme.R
-import com.sopt.smeme.business.viewmodel.ViewModelFrame
 import com.sopt.smeme.databinding.ActivityTestBinding
-import com.sopt.smeme.business.adaptor.TempAdaptor
-import dagger.hilt.android.AndroidEntryPoint
 
 // @AndroidEntryPoint
 class TestActivity : ViewBoundActivity<ActivityTestBinding>(R.layout.activity_test) {
