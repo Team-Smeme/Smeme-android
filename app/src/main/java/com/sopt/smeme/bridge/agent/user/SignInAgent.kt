33b7c3f261021d384e77fab0c81a68159173d339
스프링 피바라기 <daehwan7989@naver.com>
2023-01-07 12:53:50 +0900

    define action after login

--- app/src/main/java/com/sopt/smeme/bridge/agent/user/SignInAgent.kt
+++ app/src/main/java/com/sopt/smeme/bridge/agent/user/SignInAgent.kt
@@ -4,5 +4,5 @@ import com.sopt.smeme.SignInType
 import com.sopt.smeme.bridge.agent.Agent
 
 interface SignInAgent : Agent {
-    fun call(type: SignInType.Type)
+    fun call(type: SignInType.Type, onCompleted: () -> Unit, onError: () -> Unit)
 }
