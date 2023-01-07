067f36f0d71bc0c3b49b6f28c2e8ca552281bcde
waterminn <tnals08019@gmail.com>
2023-01-07 21:40:58 +0900

    [ui] fix hint button text color

--- app/src/main/java/com/sopt/smeme/presentation/view/odir/OdirDetailActivity.kt
+++ app/src/main/java/com/sopt/smeme/presentation/view/odir/OdirDetailActivity.kt
@@ -9,17 +9,8 @@ import com.sopt.smeme.databinding.ActivityOdirDetailBinding
 class OdirDetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityOdirDetailBinding.inflate(layoutInflater) }
 
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.btnHintOdirDetail.setOnClickListener {
-            if (binding.btnHintOdirDetail.isChecked) {
-                binding.btnHintOdirDetail.setTextColor(R.color.primary)
-            } else {
-                binding.btnHintOdirDetail.setTextColor(R.color.grey_400)
-            }
-        }
     }
 }
