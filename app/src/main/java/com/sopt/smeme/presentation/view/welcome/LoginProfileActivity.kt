458b7464e751a6578115276d2c19496cb304f2f4
스프링 피바라기 <daehwan7989@naver.com>
2023-01-08 15:22:10 +0900

    [ui] make login profile view

---
+++ app/src/main/java/com/sopt/smeme/presentation/view/welcome/LoginProfileActivity.kt
@@ -0,0 +1,9 @@
+package com.sopt.smeme.presentation.view.welcome
+
+import com.sopt.smeme.R
+import com.sopt.smeme.databinding.ActivityLoginProfileBinding
+import com.sopt.smeme.presentation.view.ViewBoundActivity
+
+class LoginProfileActivity :
+    ViewBoundActivity<ActivityLoginProfileBinding>(R.layout.activity_login_profile) {
+}
