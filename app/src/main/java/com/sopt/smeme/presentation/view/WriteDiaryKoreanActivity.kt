d9dd73f6e23c1ed7b214fe2f58ed05d4c8ab6fcc
taeheeL <t9h9@naver.com>
2023-01-08 15:00:52 +0900

    bind text and checkbox about color and listener

--- app/src/main/java/com/sopt/smeme/presentation/view/WriteDiaryKoreanActivity.kt
+++ app/src/main/java/com/sopt/smeme/presentation/view/WriteDiaryKoreanActivity.kt
@@ -1,8 +1,6 @@
 package com.sopt.smeme.presentation.view
 
-import android.annotation.SuppressLint
 import android.graphics.Color
-import android.graphics.Typeface
 import android.os.Bundle
 import android.text.SpannableStringBuilder
 import android.text.Spanned
@@ -22,8 +20,11 @@ class WriteDiaryKoreanActivity : AppCompatActivity() {
         _binding = ActivityWrteDiaryKoreanBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setColorQ()
+        setQuestionState()
         setColorTip()
+
+        setOnClickCheckbox()
+
     }
 
     private fun setColorTip() {
@@ -35,15 +36,55 @@ class WriteDiaryKoreanActivity : AppCompatActivity() {
         binding.txtTip.text = builder
     }
 
-    private fun setColorQ(){
+    private fun setQuestionState() {
         val randomTopic: String = binding.txtRandomTopic.text.toString()
         val builder = SpannableStringBuilder(randomTopic)
         val fixFont = StyleSpan(R.style.TextAppearance_Body3)
         val colorPrimary = ForegroundColorSpan(Color.parseColor("#FE9870"))
         builder.setSpan(colorPrimary, 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        builder.setSpan(fixFont,0,2,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(fixFont, 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         binding.txtRandomTopic.text = builder
     }
 
+    private fun setOnClickCheckbox() {
+        with(binding) {
+            cbRandom.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    txtRandom.setTextColor(Color.parseColor("#FE9870"))
+                } else {
+                    txtRandom.setTextColor(Color.parseColor("#A6A6A6"))
+                }
+            }
+
+            cbPublic.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    txtPublic.setTextColor(Color.parseColor("#FE9870"))
+                } else {
+                    txtPublic.setTextColor(Color.parseColor("#A6A6A6"))
+                }
+            }
+
+            txtRandom.setOnClickListener {
+                if (cbRandom.isChecked) {
+                    txtRandom.setTextColor(Color.parseColor("#A6A6A6"))
+                    cbRandom.isChecked = false
+                } else {
+                    txtRandom.setTextColor(Color.parseColor("#A6A6A6"))
+                    cbRandom.isChecked = true
+                }
+            }
+
+
+            txtPublic.setOnClickListener {
+                if (cbPublic.isChecked) {
+                    txtPublic.setTextColor(Color.parseColor("#A6A6A6"))
+                    cbPublic.isChecked = false
+                } else {
+                    txtPublic.setTextColor(Color.parseColor("#A6A6A6"))
+                    cbPublic.isChecked = true
+                }
+            }
+        }
+    }
 }
