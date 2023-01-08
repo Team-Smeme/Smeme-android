d743d05a31bf51228e1f8f5f86bc20c51768d039
taeheeL <t9h9@naver.com>
2023-01-09 02:18:00 +0900

    back to step1

--- app/src/main/java/com/sopt/smeme/presentation/view/WriteDiaryStep2Activity.kt
+++ app/src/main/java/com/sopt/smeme/presentation/view/WriteDiaryStep2Activity.kt
@@ -1,17 +1,27 @@
 package com.sopt.smeme.presentation.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.sopt.smeme.databinding.ActivityWriteKoreanStep2Binding
+import com.sopt.smeme.databinding.ActivityWriteStep2Binding
 
 class WriteDiaryStep2Activity : AppCompatActivity() {
-    private var _binding: ActivityWriteKoreanStep2Binding? = null
-    private val binding: ActivityWriteKoreanStep2Binding
+    private var _binding: ActivityWriteStep2Binding? = null
+    private val binding: ActivityWriteStep2Binding
         get() = requireNotNull(_binding) { "error in WriteDiaryKoreanActivity" }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityWriteKoreanStep2Binding.inflate(layoutInflater)
+        _binding = ActivityWriteStep2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        toStep1()
+    }
+
+    private fun toStep1() {
+        binding.btnBack.setOnClickListener {
+            val toStep1 = Intent(this, WriteDiaryStep1Activity::class.java)
+            startActivity(toStep1)
+        }
     }
 }
