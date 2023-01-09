f53b7f726d063ea64056d85847f8287fa40baee8
waterminn <tnals08019@gmail.com>
2023-01-09 15:25:33 +0900

    implement recyclerview

--- app/src/main/java/com/sopt/smeme/presentation/view/archive/ArchiveFragment.kt
+++ app/src/main/java/com/sopt/smeme/presentation/view/archive/ArchiveFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sopt.smeme.business.adaptor.ArchiveAdapter
+import com.sopt.smeme.business.viewmodel.ArchiveViewModel
 import com.sopt.smeme.databinding.FragmentArchiveBinding
 
 class ArchiveFragment : Fragment() {
+    private val viewModel by viewModels<ArchiveViewModel>()
     private var _binding: FragmentArchiveBinding? = null
     private val binding: FragmentArchiveBinding
         get() = requireNotNull(_binding) { "value of _binding is null" }
@@ -20,5 +24,18 @@ class ArchiveFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentArchiveBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArchiveAdapter(requireContext())
+        binding.rvExpressionArchive.adapter = adapter
+        adapter.setArchiveList(viewModel.archiveList)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
